7a86fbb737eb29537bd9add74e38fb89ea399d8e
mahisha2022 <mahletdrark@gmail.com>
2023-01-11 21:01:00 +0000

    Saved progress on InnerJoin 2023-01-11 21:01:00 +0000

--- InnerJoin/src/main/java/InnerJoinActivity.java
+++ InnerJoin/src/main/java/InnerJoinActivity.java
@@ -94,6 +94,10 @@ public class InnerJoinActivity {
          */
         String sql = FileUtil.parseSQLFile("problem1.sql");
 
+                sql = "SELECT  student.id, student_name, student.class_title, class.teacher_name "
+                    + "FROM student INNER JOIN class"
+                    + "ON student.class_title = class.class_title"
+                    + " WHERE student.class = 'Physics' OR student.class = 'Math' ";
 
 
         Set<Pair<Integer, String>> results = new HashSet<>();
