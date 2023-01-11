3c9b31ffad28c680cedd10fd1ff64b69a5cb41a6
mahisha2022 <mahletdrark@gmail.com>
2023-01-11 16:45:59 +0000

    Saved progress on IsSorted 2023-01-11 16:45:59 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class IsSorted {
     /**
      * Return true if arr is sorted, smallest to largest, false otherwise.
@@ -7,6 +9,13 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        //create a boolean var
+        boolean isSorted = true; 
+        //loop through the arr to sort
+        for (int i =0 ; i < arr.length -1 && isSorted; i++ ){
+            isSorted = arr[i] <= arr[i+1];
+        }
+        //return a boolean value 'isSoretd'
+        return isSorted;
     }
 }
