8e28b069620768f7ab948653eb8c311999ea608a
mahisha2022 <mahletdrark@gmail.com>
2023-01-16 04:51:04 +0000

    Saved progress on ContainsDuplicates 2023-01-16 04:51:04 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -8,6 +8,14 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+            for(int i = 0; i < nums.length; i++){
+                for(int j = i + 1; j < nums.length; j++){
+                    if(i != j && nums[i] == nums[j]){
+                        return true;
+                    }
+                }
+            }
+
         return false;
     }
 }
