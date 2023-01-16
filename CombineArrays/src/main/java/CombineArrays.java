0edfb12c932794004aca6c3f94c9e688d974fb2d
mahisha2022 <mahletdrark@gmail.com>
2023-01-16 05:24:02 +0000

    Saved progress on CombineArrays 2023-01-16 05:24:02 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import javafx.print.Collation;
 
 public class CombineArrays {
     /**
@@ -8,7 +12,22 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        // first combine the two arr length together before combine the elements
+        int length = arr1.length + arr2.length;
+        int [] result = new int[length];
+        // declare a variable to assign a position for the elements
+        int position = 0;
+        
+        //loop through the arr1 & arr2 to copy the elements 
+        for(int e : arr1){
+            result[position] = e;
+            position ++;
+        }
+        for(int e : arr2){
+            result[position] = e;
+            position++;
+        }
 
-        return null;
+        return result;
     }
 }
