24425be10eb6156c811f6fec0a7a8994946adffc
mahisha2022 <mahletdrark@gmail.com>
2023-01-20 04:08:22 +0000

    Saved progress on WordCount 2023-01-20 04:08:22 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,6 +5,14 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        int count = 0; 
+        char newchar [] = new char[in.length()];
+        for(int i = 0; i< in.length(); i++){
+            newchar[i] = in.charAt(i);
+            if( (( i > 0) && (newchar[i] != ' ') && (newchar[i-1] == ' ')) || ((newchar[0] != ' ') && (i == 0)) )  
+            count++; 
+        }
+
+        return count;
     }
 }
