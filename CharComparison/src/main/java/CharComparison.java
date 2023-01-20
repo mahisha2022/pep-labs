a4655e932b5479f448ce3cd50a6d01cd2daaca72
mahisha2022 <mahletdrark@gmail.com>
2023-01-20 15:49:30 +0000

    Saved progress on CharComparison 2023-01-20 15:49:30 +0000

--- CharComparison/src/main/java/CharComparison.java
+++ CharComparison/src/main/java/CharComparison.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CharComparison {
     /**
      * Given an array of char, determine if the array A belongs to the left or to the right of char array B lexigraphically.
@@ -11,6 +13,16 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
+       
+        for(int i = 0; i < a.length && i < b.length; i++){
+           
+            if(a[i] < b[i]){
+                return -1;
+            }
+            if(a[i] > b[i]){
+                return 1; 
+            }
+        }
         return 0;
     }
 }
