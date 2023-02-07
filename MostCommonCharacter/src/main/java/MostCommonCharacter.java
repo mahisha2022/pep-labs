ca7ef5ec79f19e69ac5a9384a139c348f590dbb4
mahisha2022 <mahletdrark@gmail.com>
2023-02-07 02:17:42 +0000

    Saved progress on MostCommonCharacter 2023-02-07 02:17:42 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -11,6 +11,25 @@ public class MostCommonCharacter {
      */
     public char recurringChar(String str) {
         
-      
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i <str.length(); i++){
+            char ch = str.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch) + 1);
+            }else {
+                map.put(ch, 1);
+            }
+        }
+        int max = 0;
+        char maxOccuringChar = ' ';
+        for (Map.Entry<Character, Integer> entry: map.entrySet()){
+            if(entry.getValue() > max){
+                max = entry.getValue();
+                maxOccuringChar = entry.getKey();
+            }
+        }
+        return maxOccuringChar;
+
     }
+    
 }
